b865d4a72f587b1cdef0f5a13f813c3dd6cc68ba
sadegh1998 <sadegh.mazaheri1377@gmail.com>
2024-12-22 22:32:43 +0330

    Create web and mobile app in presentation layer.

---
+++ Modirsa/Modirsa.Presentation/Modirsa.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Modirsa.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Modirsa/Modirsa.Presentation/Modirsa.Mobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Modirsa.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
